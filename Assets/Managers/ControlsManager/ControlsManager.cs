357a83609ce1a4c312a46c7996badb87c16c16e1
DevJacek112 <jacekteodorczyk01@gmail.com>
2023-08-06 16:52:14 +0200

    Dodanie systemu wsiadania i wysiadania pasażerów.

--- Assets/Managers/ControlsManager/ControlsManager.cs
+++ Assets/Managers/ControlsManager/ControlsManager.cs
@@ -6,10 +6,12 @@ using UnityEngine.InputSystem;
 public class ControlsManager : MonoBehaviour
 {
     [SerializeField] private PauseManager _pauseManager;
+    [SerializeField] private BusSystems _busSystems;
 
     public Controls Controls;
 
     private InputAction _escapePressedAction;
+    private InputAction _EpressedAction;
 
     private void Awake()
     {
@@ -21,15 +23,25 @@ public class ControlsManager : MonoBehaviour
         _escapePressedAction = Controls.UI.Cancel;
         _escapePressedAction.Enable();
         _escapePressedAction.performed += EscapePressed;
-    }
 
+        _EpressedAction = Controls.Player.DoorControl;
+        _EpressedAction.Enable();
+        _EpressedAction.performed += _Epressed;
+    }
+    
     private void OnDisable()
     {
         _escapePressedAction.Disable();
+        _EpressedAction.Disable();
     }
     
     private void EscapePressed(InputAction.CallbackContext context)
     {
         _pauseManager.PauseButtonPressed();
+    }
+
+    private void _Epressed(InputAction.CallbackContext obj)
+    {
+        _busSystems.DoorsAction();
     }
 }
